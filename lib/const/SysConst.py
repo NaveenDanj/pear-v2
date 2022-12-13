d2132dfb655430d25f5d0a2ed68aa472e8353783
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2022-12-13 19:43:25 +0530

    added conditional statement lexer

--- lib/const/SysConst.py
+++ lib/const/SysConst.py
@@ -4,3 +4,12 @@ class Line:
         self.raw_line = line
         self.line_number = lm
         self.line = []
+
+class Statement:
+
+    def __init__(self , raw_statement , splitted ):
+        self.nextStatement = None
+        self.raw_statement = raw_statement
+        self.splitted = splitted
+        self.identifier = None
+        self.line_number = None
