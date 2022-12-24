27ac280bf08cf5d12570f0a57b0bd959952f1362
Naveen Hettiwaththa <naveenhettiwaththa@gmail.com>
2022-12-24 12:51:34 +0530

    fixed wrong function call issue

--- lib/RuntimUtil/function_util.py
+++ lib/RuntimUtil/function_util.py
@@ -1,17 +1,29 @@
 
 
 def handle_function_call(statement , parse_tree):
-    
+
+    temp_pointer = None
+    if statement.next != None:
+        temp_pointer = statement.next.pointer
+
     function_name = statement.raw_statement.split(' ' , 1)[1]
     function_name = function_name[:function_name.index('>')-1]
+    function_name = function_name.strip()
     function_start_pointer = None
+
     for index , st in enumerate(parse_tree):
         if st.splitted[0] == 'function':
-            function_start_pointer = parse_tree[index+1 ]
             func_name = st.raw_statement.split(" ")[1]
-            if func_name == function_name:
+            func_name = func_name.strip()
+            if check_func_name_matching(func_name , function_name):
+                function_start_pointer = parse_tree[index+1 ]
                 st.default_pointer.next = statement.next
                 statement.next = st.next
+        elif st.splitted[0] == 'endfunction':
+            if temp_pointer == None:
+                st.next = None
+            else:
+                st.next = parse_tree[temp_pointer]
 
     if function_start_pointer == None:
         raise Exception('Function name '+ function_name +'undefined!')
@@ -20,3 +32,19 @@ def handle_function_call(statement , parse_tree):
 
 def handle_function_init(statement):
     return statement.next
+
+
+def handle_endfunction(statement , parse_tree):
+    print('hit the end function')
+    return statement.next
+
+def check_func_name_matching(n1 , n2):
+
+    if len(n1) != len(n2):
+        return False
+    
+    for i in range(len(n1)-1):
+        if n1[i] != n2[i]:
+            return False
+
+    return True
--- lib/Runtime.py
+++ lib/Runtime.py
@@ -3,7 +3,7 @@ from lib.RuntimUtil.Mem import var
 from lib.RuntimUtil.var_util import handle_var_statement , handle_set_var_statement
 from lib.RuntimUtil.print_util import handle_print
 from lib.RuntimUtil.while_util import handle_while
-from lib.RuntimUtil.function_util import handle_function_call , handle_function_init
+from lib.RuntimUtil.function_util import handle_function_call , handle_function_init , handle_endfunction
 
 def run_pointer_statement(statement , parse_tree):
     if statement.splitted[0] == 'if':
@@ -18,9 +18,12 @@ def run_pointer_statement(statement , parse_tree):
         return handle_print(statement)
     elif statement.splitted[0] == 'call':
         next_ , parse_tree = handle_function_call(statement , parse_tree)
+        # print('call statement : ' , statement.raw_statement , 'call next : ' , next_.raw_statement)
         return next_
     elif statement.splitted[0] == 'function':
         return handle_function_init(statement)
+    elif statement.splitted[0] == 'endfunction':
+        return handle_endfunction(statement , parse_tree)
     else:
         return statement.next
 
